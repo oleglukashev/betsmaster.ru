b852372dcaf828c99549e6d1bef38c924feea792
Oleg Lukashev <incognitorus@gmail.com>
2012-04-25 18:14:40 +0400

    add content box html

--- application/classes/controller/content.php
+++ application/classes/controller/content.php
@@ -1,16 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Content extends Controller_Main {
-    
-    public $template = 'content/inner';
-    
+
+    public $template = 'content/main';
+
     public function action_index()
     {
         $this->model = new Model_Content();
 
         $url = $this->request->param('url');
-        echo $url;
-        print_r($this->getContent($url));
         $this->template->content = $this->getContent($url);
     }
 
